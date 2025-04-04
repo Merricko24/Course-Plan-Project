1a6edec4b235caf8787e530e94c8efd1ae46bd51
Coleman Caldwell <99365010+ccald27@users.noreply.github.com>
2025-04-04 16:42:11 -0600

    added JSSoup scraping func

--- ProjectSourceCode/class_scraper.js
+++ ProjectSourceCode/class_scraper.js
@@ -1,11 +1,17 @@
 const axios = require('axios')
 var JSSoup = require('jssoup').default;
 
+//importance in API requests. We can use this to get 
 axios.get('https://classes.colorado.edu/api/?page=fose&route=search&keyword=CSCI')
 
     .then((response) => {
         //console.log(response.data) // debug
         console.log(response)
+        const soup = new JSSoup(response.data);
+        //console.log(soup) // debug
+        const classes = soup.findAll('div', 'course');
+        //console.log(classes) // debug
+        const classList = [];
         
 
     })
