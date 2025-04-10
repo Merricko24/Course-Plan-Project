185aa8e2d5fb0c757215c94cfd57f470dc6bd01e
Merricko24 <merrick.oleszek@colorado.edu>
2025-04-10 12:53:21 -0600

    updated register submit paths

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -7,7 +7,7 @@ const pgp = require('pg-promise')();
 const bodyParser = require('body-parser');
 const session = require('express-session');
 // const bcrypt = require('bcrypt'); // Added bcrypt for password hashing
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + ''));
 
 // -------------------------------------  APP CONFIG   ----------------------------------------------
 
