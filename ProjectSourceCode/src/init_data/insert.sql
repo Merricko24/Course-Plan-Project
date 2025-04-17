9f49a6edf5b4e3af141ea7af77c071954b4bdafc
unknown <calvin.hawks@colorado.edu>
2025-04-16 22:19:09 -0600

    Commit to test search function

--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -2,8 +2,8 @@
 -- INSERT INTO students(identikey, password, first_name, last_name, email, year, advisor_id, taken_courses) VALUES 
 --     ('mann2000', '$2a$10$9nMrE/.5V0iVuDfeMvt1IeOOBjCWwZe8olbtdgqb3WnxmUfRACa7y', 'man', 'name', 'test@colorado.edu', 'Senior',  'umar1234', ARRAY[]::VARCHAR(50)[]),
 --     ('joab1234', '$2a$10$J54.q/9R8ohVOM/e22z5FOxZp/CWHfyHfK0r14c0ew81q1yNFDQyy','John', 'Applebee', 'joab1234@colorado.edu', 'Freshman',  'umar1234', ARRAY['CSCI1300']::VARCHAR(50)[]),
---     ('coca9230','$2a$10$J54.q/9R8ohVOM/e22z5FOxZp/CWHfyHfK0r14c0ew81q1yNFDQyy', 'Coleman', 'Caldwell', 'coca9230@colorado.edu', 'Senior',  'eche1337', ARRAY['CSCI1300', 'CSCI2270']::VARCHAR(50)[]),
---     ('bpit2018', '$2a$10$J54.q/9R8ohVOM/e22z5FOxZp/CWHfyHfK0r14c0ew81q1yNFDQyy','Brad', 'Pitt', 'bpit2018@colorado.edu', 'Sophomore', 'eche1337', ARRAY[]::VARCHAR(50)[]);
+    -- ('coca9230','$2a$10$J54.q/9R8ohVOM/e22z5FOxZp/CWHfyHfK0r14c0ew81q1yNFDQyy', 'Coleman', 'Caldwell', 'coca9230@colorado.edu', 'Senior',  'eche1337', ARRAY['CSCI1300', 'CSCI2270']::VARCHAR(50)[]),
+    -- ('bpit2018', '$2a$10$J54.q/9R8ohVOM/e22z5FOxZp/CWHfyHfK0r14c0ew81q1yNFDQyy','Brad', 'Pitt', 'bpit2018@colorado.edu', 'Sophomore', 'eche1337', ARRAY[]::VARCHAR(50)[]);
 
 
 -- INSERT INTO advisors (identikey, password, first_name, last_name, email, student_ids) VALUES 
